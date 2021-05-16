cb312476d4f83693bfe676b85fdf5222222c2ffa
Ziggy Moens <ziggy.moens@endare.com>
2021-05-16 09:48:24 +0200

    updated views

--- iOS Native Basic/ViewController.swift
+++ iOS Native Basic/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        label.text = "Hello, " + UIDevice.current.systemName + " " + UIDevice.current.systemVersion
     }
 
 
